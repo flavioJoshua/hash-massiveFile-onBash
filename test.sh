ae0715f96f18b7d7b5c8cbd1852f37fe2c97e4ed
flavio.joshua@gmail.com <flavio.joshua@gmail.com>
2021-09-26 00:25:18 +0200

    sesto

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 test=qualcosa 
 #echo $test
 filepath=$(zenity --file-selection   --save --title=" segli il tuo fileName esiste o nuovo")
-test -a $filepath  && echo  "ok  proseguo"  ||   echo  "KO non   proseguo"      
-[ -a $filepath ] &&  zenity  --warning --text="stai cancellando  i dati presenti nel file : "  $filepath ||  echo  "ok  proseguo"
+#test -a $filepath  && echo  "ok  proseguo"  ||   echo  "KO non   proseguo"      
+[ -a $filepath ] &&  zenity  --warning --text="stai cancellando  i dati presenti nel file : "  $filepath; rm $filepath ||  echo  "ok  proseguo"
 #sleep 1
 echo  "test"
