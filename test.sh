ff4698c694f5f3794654686a4294f5621bf399b0
flavio.joshua@gmail.com <flavio.joshua@gmail.com>
2021-09-25 23:30:05 +0200

    quinto

--- test.sh
+++ test.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 #test -a /etc/systemd/
 test=qualcosa 
-echo $test
-zenity  --warning --text="test"
-if [file exists $filepath ]
-    then
-        echo "  esiste "
-    else
-        echo  " non esiste "
-        
-fi
-#[ -a $filepath ] &&  zenity  --warning --text="stai cancellando  i dati presenti nel file : "  $filepath ||  echo  "ok  proseguo"
+#echo $test
+filepath=$(zenity --file-selection   --save --title=" segli il tuo fileName esiste o nuovo")
+test -a $filepath  && echo  "ok  proseguo"  ||   echo  "KO non   proseguo"      
+[ -a $filepath ] &&  zenity  --warning --text="stai cancellando  i dati presenti nel file : "  $filepath ||  echo  "ok  proseguo"
 #sleep 1
 echo  "test"
